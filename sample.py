7232064653948376cb6b0256f64f7f0d53734f47
jocelynn11 <kyriepham2@gmail.com>
2018-12-02 21:46:09 +0700

    btvn

---
+++ sample.py
@@ -0,0 +1,11 @@
+month = int(input("month=?"))
+
+
+if 0< month <4:
+    print("spring")
+if 3< month <7:
+    print("summer")
+if 6< month <10:
+    print("autumn")
+if 9< month <13:
+    print('winter')
