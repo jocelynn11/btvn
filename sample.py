b4d008700787e084fe9da07443d92f6c2dd595b7
jocelynn11 <45123946+jocelynn11@users.noreply.github.com>
2018-12-04 18:14:58 +0700

    Update sample.py

--- sample.py
+++ sample.py
@@ -3,9 +3,9 @@ month = int(input("month=?"))
 
 if 0< month <4:
     print("spring")
-if 3< month <7:
+elif 3< month <7:
     print("summer")
-if 6< month <10:
+elif 6< month <10:
     print("autumn")
-if 9< month <13:
+else:
     print('winter')
